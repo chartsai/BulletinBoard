2f29585b0f68946fc3a19342b121ed291a36b0a0
chatea <cha122977@gmail.com>
2015-12-26 19:10:35 +0800

    Initial Commit

---
+++ BulletinBoard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BulletinBoard
+//
+//  Created by CHA-MBP on 2015/12/26.
+//  Copyright © 2015年 bulletin board. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
